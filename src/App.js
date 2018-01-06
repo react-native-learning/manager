26b8c564d83768608c020078ed968b4aa219eda0
Doan The Anh <doantheanh172tb@gmail.com>
2018-01-07 01:07:43 +0700

    initializeApp firebase

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import {
   Text,
   View
 } from 'react-native';
+import firebase from 'firebase';
+
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 import reducers from './reducers';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBXpPjDs7UNp2ydi2vAr9VHmbtepq1Xs-c",
+      authDomain: "react-native-2b57f.firebaseapp.com",
+      databaseURL: "https://react-native-2b57f.firebaseio.com",
+      projectId: "react-native-2b57f",
+      storageBucket: "",
+      messagingSenderId: "1033782716030"
+    });
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
